bc936d3a96e36fdd6fe0f095749786c9aa6cf7bf
pekka <pekka@vihakone>
2025-11-17 17:36:58 +0200

    Revert "Geany theme, scripts for hyprlock screen."
    This reverts commit 1605836fe8b7687247993d2bdb32a9c7db67c820.

--- pacman.sh
+++
@@ -1,1 +0,0 @@
-sudo pacman -Syu wallust yazi stow cava tlp delfin bitwarden qownnotes freetube hyprpaper hypridle hyprlock ttf-orbitron ttf-jetbrains-mono ttf-rajdhani ttf-exo ttf-audiowide oldschool-pc-fonts
