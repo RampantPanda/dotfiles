1605836fe8b7687247993d2bdb32a9c7db67c820
pekka <pekka@vihakone>
2025-11-17 16:39:47 +0200

    Geany theme, scripts for hyprlock screen.

---
+++ pacman.sh
@@ -0,0 +1,1 @@
+sudo pacman -Syu wallust yazi stow cava tlp delfin bitwarden qownnotes freetube hyprpaper hypridle hyprlock ttf-orbitron ttf-jetbrains-mono ttf-rajdhani ttf-exo ttf-audiowide oldschool-pc-fonts
